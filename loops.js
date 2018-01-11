4aa2c391e01bc91f4bf45e4c9006826a9f955181
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-11 02:35:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 function forLoop(forlooparray){
   for (let i = 0; i<25; i++) {
       if (i === 1) {
+<<<<<<< HEAD
         forlooparray.push("I am 1 strange loop.");
       }
       else {
@@ -8,6 +9,14 @@ function forLoop(forlooparray){
       }
   }
   return forlooparray
+=======
+        forlooparray[i] = "I am 1 strange loop.";
+      }
+      else {
+        forlooparray[i] = `I am ${i} strange loops.`;
+      }
+  }
+>>>>>>> eafdbfd792acae4f3dbffe1788ebbfd522be2955
 }
 
 function whileLoop (counter) {
@@ -24,8 +33,14 @@ function maybeTrue() {
 
 function doWhileLoop (array) {
   do {
+<<<<<<< HEAD
     array.shift()
     console.log(array)
   }   while (maybeTrue() && array.length > 0);
   return array
 }
+=======
+    array.pop();
+  }   while (maybeTrue() && array.length > 0);
+}
+>>>>>>> eafdbfd792acae4f3dbffe1788ebbfd522be2955
